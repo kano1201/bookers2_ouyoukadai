e47617a36ee5b8b5e06ed399f657b7a70661e5e4
kano1201 <kano.nakamura3@gmail.com>
2021-07-03 12:41:59 +0000

    follow

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,23 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   attachment :profile_image, destroy: false
 
+  has_many :relationships
+  has_many :active_relationships, class_name:"Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :active_relationships, source: :followed
+
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :relationships, source: :follower
+
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  def following?(user)
+    followings.include?(user)
+  end
+
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
 end
