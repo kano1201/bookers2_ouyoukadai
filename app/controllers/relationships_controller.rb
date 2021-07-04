506f4d6407746e08b2af3382185bd031fc3cc983
kano1201 <kano.nakamura3@gmail.com>
2021-07-04 02:57:54 +0000

    fix_follow

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,17 +1,20 @@
 class RelationshipsController < ApplicationController
+
   def create
-    current_user.follow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    current_user.follow!(@user)
+    redirect_to @user
   end
 
   def destroy
-    current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    @user = User.find(params[:user_id])
+    current_user.unfollow!(@user)
+    redirect_to @user
   end
 
   def followings
     user = User.find(params[:user_id])
-    @users = user.followings
+    @users = user.followed_users
   end
 
   def followers
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,21 +9,22 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   attachment :profile_image, destroy: false
 
-  has_many :relationships
-  has_many :active_relationships, class_name:"Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :followers, through: :active_relationships, source: :followed
-
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followings, through: :relationships, source: :follower
+  has_many :followed_users, through: :relationships, source: :followed
 
-  def follow(user_id)
-    relationships.create(followed_id: user_id)
+  has_many :reverse_relationships, class_name:"Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+
+  def following?(other_user)
+    relationships.find_by(followed_id: other_user.id)
   end
-  def unfollow(user_id)
-    relationships.find_by(followed_id: user_id).destroy
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
   end
-  def following?(user)
-    followings.include?(user)
+
+  def unfollow!(other_user)
+    relationships.find_by(followed_id: other_user.id).destroy
   end
 
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
