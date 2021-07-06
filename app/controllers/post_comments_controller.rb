3508bb27790403658421a55fabf1cfe3bbfdf5f3
kano1201 <kano.nakamura3@gmail.com>
2021-07-06 12:55:00 +0000

    finish_task6

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -5,21 +5,21 @@ class PostCommentsController < ApplicationController
     @post_comment = PostComment.new(post_comment_params)
     @post_comment.book_id = @book.id
     @post_comment.user_id = current_user.id
-    respond_to do |format|
-    if @post_comment.save
-      format.html {redirect_to @book}
-      format.js {render 'books/comment'}
-    else
-      format.html {redirect_to @book}
-    end
-    end
+    @post_comment.save
+    #respond_to do |format|
+    #if @post_comment.save
+      #format.html {redirect_to @book}
+      #format.js {render 'books/comment'}
+    #else
+      #format.html {redirect_to @book}
+    #end
+    #end
   end
 
   def destroy
 		@book = Book.find(params[:book_id])
-  	post_comment = @book.post_comments.find(params[:id])
-		post_comment.destroy
-		redirect_to request.referer
+  	@post_comment = @book.post_comments.find(params[:id])
+		@post_comment.destroy
   end
 
   private
