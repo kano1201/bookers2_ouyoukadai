0a6e2445f805906df14758a9c2f537fb83893765
kano1201 <kano.nakamura3@gmail.com>
2021-07-08 04:23:45 +0000

    ランキング昇順

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
 	has_many :post_comments, dependent: :destroy
 	attachment :profile_image, destroy: false
 	has_many :favorites, dependent: :destroy
+	has_many :favorited_users, through: :favorites, source: :user
 
 	def favorited_by?(user)
 		favorites.where(user_id: user.id).exists?
